9045b69ac3105a6be1f20db70c30b78979f19fb6
gustavomendess <gustavocod13@outlook.com>
2021-08-17 00:33:46 -0300

    Realiza testes básicos com cálculos

---
+++ spec/calculator/calculator_spec.rb
@@ -0,0 +1,29 @@
+require 'calculator'
+
+describe Calculator do
+  context '#sum' do
+    it 'with positive numbers' do
+      result = subject.sum(5, 7)
+      expect(result).to eq(12)
+    end
+  
+    it 'with negative and positive numbers' do
+      result = subject.sum(-5, 7)
+      expect(result).to eq(2)
+    end
+
+    it 'with negative numbers' do
+      result = subject.sum(-5, -7)
+      expect(result).to eq(-12)
+    end
+  end
+
+  context '#div' do
+    it 'divide by zero' do
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError)
+      expect{subject.div(3, 0)}.to raise_error('divided by 0')
+      expect{subject.div(3, 0)}.to raise_error(ZeroDivisionError, 'divided by 0')
+      expect{subject.div(3, 0)}.to raise_error(/divided/)
+    end
+  end
+end 
